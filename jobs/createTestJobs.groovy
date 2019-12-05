7cf578229bd94ca12740bcfc7fa23c5aa2ee7cde
Sebastian Wenta <sebastian.wenta@uniteam.pl>
2019-12-05 14:06:02 +0100

    path to configuration file changed

--- jobs/createTestJobs.groovy
+++ jobs/createTestJobs.groovy
@@ -8,7 +8,7 @@ def jsonSlurper = new JsonSlurper()
 def environment = "TEST"
 def confgurationData = jsonSlurper.parse(new File("$WORKSPACE/src/resources/configuration.json")).Environments.find {it.env==environment}
 
-println("JIRA - " + params.toString())
+println("JIRA - " + jira)
 
 if (confgurationData==null) {
     println "No configuration data for environment $environment"
