be8b84f542a83573a9941b5a6593063a57411f2d
Sebastian Wenta <sebastian.wenta@uniteam.pl>
2019-12-09 15:20:24 +0100

    path to configuration file changed

--- jobs/createTestJobs.groovy
+++ jobs/createTestJobs.groovy
@@ -111,7 +111,7 @@ testScripts.eachWithIndex{ String part, int index ->
             stage ('Run next job') {
                 steps{
                     step{
-                        build job: ''' + testPlanName + '''_''' + index + 1 + '''
+                        build job: \'''' + testPlanName + '''_''' + (index + 1) + '''\'
                     }
                 }
             }
